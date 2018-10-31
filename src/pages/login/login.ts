6817ddca918a9bd1b5c3ed2b28b50fe0061640ab
unknown <you@example.com>
2018-10-31 14:31:23 -0600

    se conecta al servidor /home/

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 //import {HomePage} from "../../../../prueba/src/pages/home/home";
 import {HomePage} from "../home/home";
+import {Http} from "@angular/http";
 
 /**
  * Generated class for the LoginPage page.
@@ -18,8 +19,13 @@ import {HomePage} from "../home/home";
 export class LoginPage {
 
   home = HomePage;
+  usuario = '';
+  password = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public http: Http) {
+
   }
 
   ionViewDidLoad() {
@@ -28,9 +34,20 @@ export class LoginPage {
 
   clickHome(){
     console.log("clickHome");
+    console.log(this.usuario);
+    console.log(this.password);
+
+    //url que da el hello world, lo tine que autocompletar solo, error atras de la flechita va subrayado
+    this.http.get('/home/')
+      .suscribe(  next: data => {
+        console.log(data.text());
+      }, error:error =>{
+        console.log("error");
+      }
+    );
 
     //this.navCtrl.push(this.home);
-    this.navCtrl.setRoot(this.home);
+    //this.navCtrl.setRoot(this.home);
 
   }
 
